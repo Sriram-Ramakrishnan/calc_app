1a19c0ac7112b690da97823785f46b01b5cf7675
Sriram-Ramakrishnan <srirampro107@gmail.com>
2022-04-14 12:04:46 +0530

    Added brackets, power and few constants

--- main.js
+++ main.js
@@ -10,18 +10,27 @@ inputs.forEach(inp => {
     if(text == "AC"){
             screenvalue = '';
             answer.value = screenvalue;
-        }
-        else if(text == "="){
+    }
+    else if(text == "="){
             answer.value = eval(screenvalue);
-        }
-        else if(text == "DEL"){
+            screenvalue = answer.value; // To work with the number instead of the expression
+    }
+    else if(text == "DEL"){
             answer.value = answer.value.substr(0, answer.value.length - 1);
             screenvalue = answer.value;
+    }
+    else{
+        if (text == "^"){
+            screenvalue+="**";}
+        else if (text == "π"){
+            screenvalue+="3.14";
+        }
+        else if (text == "e"){
+            screenvalue+="2.718"
         }
         else{
-            screenvalue += text;
-            answer.value = screenvalue;
-            
+            screenvalue += text;}
+        answer.value = screenvalue;
         }
         
     })
